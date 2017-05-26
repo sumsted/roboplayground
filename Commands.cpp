656bce6cf4bc706e9bffecf58ae9fcc4e3615039
Scott Umsted <scottumsted@gmail.com>
2017-05-26 08:00:51 -0500

    adding missing declaration and fixing typos

--- Commands.cpp
+++ Commands.cpp
@@ -164,6 +164,6 @@ void Commands::i2c_command(byte command, byte payload, byte (&cp)[2]){
     }
 }
 
-void Commands:i2c_master_command(byte command, byte payload){
-  I2CLink.master_send_data(command, payload);
+void Commands::i2c_master_command(byte command, byte payload){
+  I2CLink::master_send_data(command, payload);
 }
